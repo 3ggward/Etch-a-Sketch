685d99bc2705e44f66ee05644cb4992985a45960
Edward <edwardtliu8@gmail.com>
2024-03-30 23:45:53 -0700

    Changed color selection from just blue, to choosing a random color each time cursor hovers over a grid cell

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let container;
 let button;
+let color;
 
 window.addEventListener("DOMContentLoaded", () => { 
     container = document.querySelector(".container")
@@ -18,9 +19,7 @@ function createGrid(size) {
 
         for (j = 0; j < size; j++) {
             let cell = document.createElement("div");
-            cell.onmouseenter = function(){
-                cell.style.backgroundColor = "blue";
-            }
+            cell.onmouseenter = () => changeColor(cell);
             cell.classList.add("cell");
             cell.style.width = dim + "px";
             cell.style.height = dim + "px";
@@ -46,4 +45,19 @@ function clearGrid() {
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomRgb() {
+    let r = getRandomInt(255);
+    let g = getRandomInt(255);
+    let b = getRandomInt(255);
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function changeColor(cell) {
+    cell.style.backgroundColor = getRandomRgb();
 }
